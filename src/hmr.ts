773fb59251a377d650a162badffdcb35dd34f67a
jsongo <jsongo@qq.com>
2017-12-08 15:56:53 +0800

    update

--- src/hmr.ts
+++ src/hmr.ts
@@ -5,6 +5,7 @@ export const hmrBootstrap = function(
         module: any,
         bootstrap: () => Promise<NgModuleRef<any>>) {
     let ngModule: NgModuleRef<any>;
+    // console.log(module.hot);
     if (module.hot) {
         module.hot.accept();
         bootstrap().then((mod) => {
@@ -12,7 +13,7 @@ export const hmrBootstrap = function(
         });
         module.hot.dispose(() => {
             const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
-            const elements = appRef.components.map(c => c.location.nativeElement);
+            const elements = appRef.components.map((c) => c.location.nativeElement);
             const makeVisible = createNewHosts(elements);
             ngModule.destroy();
             makeVisible();
